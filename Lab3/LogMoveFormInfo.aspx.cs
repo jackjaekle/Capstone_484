2e0ef33c0554b8be6d30fd3a6efaa7f05c8042be
manyxcm <38437169+manyxcm@users.noreply.github.com>
2021-03-12 17:32:43 -0500

    Add files via upload

---
+++ Lab3/LogMoveFormInfo.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab3
+{
+    public partial class LogMoveFormInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            String sqlQuery = "Select * FROM MoveForm WHERE ServiceID = " + ddlMoveForm.SelectedValue;
+            String sqlQuery2 = "SELECT * from Rooms WHERE MoveFormID = " + ddlMoveForm.SelectedValue;
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
+
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnect;
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.CommandText = sqlQuery;
+
+            SqlCommand sqlCommand2 = new SqlCommand();
+            sqlCommand2.Connection = sqlConnect;
+            sqlCommand2.CommandType = CommandType.Text;
+            sqlCommand2.CommandText = sqlQuery2;
+
+            sqlConnect.Open();
+            SqlDataReader queryResults = sqlCommand.ExecuteReader();
+            grdMoveForm.DataSource = queryResults;
+            grdMoveForm.DataBind();
+            sqlConnect.Close();
+
+            sqlConnect.Open();
+            SqlDataReader query2Results = sqlCommand2.ExecuteReader();
+            grdRoomInfo.DataSource = query2Results;
+            grdRoomInfo.DataBind();
+            sqlConnect.Close();
+
+            lblMoveForm.Text = "MoveForm information for MoveForm " + ddlMoveForm.SelectedValue;
+            lblRoomInfo.Text = "Rooms Information for MoveForm " + ddlMoveForm.SelectedValue;
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LoggedinMainPage.aspx");
+        }
+    }
+}
