48b066cd805627d29f7fd88bec1b271d4fd11a26
manyxcm <38437169+manyxcm@users.noreply.github.com>
2021-03-12 17:26:47 -0500

    Add files via upload

---
+++ LoggedinMainPage.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LoggedinMaster.Master" AutoEventWireup="true" CodeFile="LoggedinMainPage.aspx.cs" Inherits="Lab1.LoggedinMainPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <!--Noah  George, William Kilpatrick, Henry Requeno-Villeda  2/15/2021 -->
+        <div>
+                Main Menu<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Information&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Create New&nbsp; Record&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Edit / Update Records&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Customer Requests<br />
+                <br />
+            <%-- Creates all the required buttons  --%><%--the page for people who are logged in--%>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="CustomerInfo" runat="server" Text="Customer Info" OnClick="CustomerInfo_Click" Width="130px" />
+                <asp:Button ID="newAuction" runat="server" Text="New Auction Event" OnClick="newAuction_Click" Width="194px"/>
+            <asp:Button ID="noteMgmt" runat="server" Text="Note Management" OnClick="noteMgmt_Click" Width="160px"/>
+                <asp:Button ID="servReq" runat="server" Text="Customer Service Requests" OnClick="servReq_Click" Width="312px" />
+                <asp:Label ID="customersWait" runat="server" Text=""></asp:Label>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="EmployeeInfo" runat="server" Text="Employee Info" OnClick="EmployeeInfo_Click" Width="130px" />
+            
+            <asp:Button ID="NewEmployee" runat="server" Text="New Employee" OnClick="NewEmployee_Click" Width="194px" Height="26px" />
+
+            <asp:Button ID="serviceEdit" runat="server" Text="Service Ticket Edit" OnClick="serviceEdit_Click" Width="160px" />
+
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="InventoryInfo" runat="server" Text="Inventory info" OnClick="InventoryInfo_Click" Width="130px" />
+            <asp:Button ID="NewItem" runat="server" Text="Add Item to Service Inventory" OnClick="NewItem_Click" Width="194px" />
+            <asp:Button ID="equipMana" runat="server" Text="Equipment Management" Width="160px" OnClick="equipMana_Click" />
+            
+                <asp:Button ID="invAuction" runat="server" Text="Customer Inventories Waiting to be sent to Auction" OnClick="invAuction_Click" Width="312px"/>
+                <asp:Label ID="auctionWait" runat="server" Text=""></asp:Label>
+                <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="moveFormBtn" runat="server" Text="MoveForm Info" OnClick="moveFormBtn_Click" Width="130px"/>
+
+            <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ServiceInfo" runat="server" Text="Service Info" Width="130px" OnClick="ServiceInfo_Click" />
+
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="WorkFlowInfo" runat="server" Text="WorkFlow Info / History" OnClick="WorkFlowInfo_Click" Width="160px" />
+            <br />
+            
+            <br />
+
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <br />
+                <br />
+            
+        </div>
+
+</asp:Content>
---
+++ LoggedinMainPage.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace Lab1
+{
+    public partial class LoggedinMainPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            String sqlQuery = "Select CustomerName FROM Customer WHERE ServicedYN = @n AND descriptionOfNeeds IS NOT Null";
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
+            String n = "0";
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Parameters.AddWithValue("n", n);
+            sqlCommand.Connection = sqlConnect;
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.CommandText = sqlQuery;
+
+            sqlConnect.Open();
+            SqlDataReader queryResults = sqlCommand.ExecuteReader();
+            int i = 0;
+
+            while (queryResults.Read())
+            {
+                i++;
+            }
+            queryResults.Close();//closes connection
+            sqlConnect.Close();
+            if (i == 1)
+            {
+                customersWait.Text = i + " Customer waiting on service";
+            }
+            else
+            {
+                customersWait.Text = i + " Customers waiting on service";
+            }
+
+            String sqlQuery1 = "Select ServiceName FROM Service WHERE AuctionedYN = @n";
+            SqlConnection sqlConnect1 = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
+            String y = "0";
+            SqlCommand sqlCommand1 = new SqlCommand();
+            sqlCommand1.Parameters.AddWithValue("n", y);
+            sqlCommand1.Connection = sqlConnect1;
+            sqlCommand1.CommandType = CommandType.Text;
+            sqlCommand1.CommandText = sqlQuery1;
+
+            sqlConnect1.Open();
+            SqlDataReader queryResults1 = sqlCommand1.ExecuteReader();
+            int x = 0;
+
+            while (queryResults1.Read())
+            {
+                x++;
+            }
+            queryResults1.Close();//closes connection
+            sqlConnect1.Close();
+            if (x == 1)
+            {
+                auctionWait.Text = x + " Customer Inventory waiting to be auctioned";
+            }
+            else
+            {
+                auctionWait.Text = x + " Customer Inventories waiting to be auctioned";
+            }
+        
+
+
+
+
+    }
+
+        protected void NewCustomer_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogNewCustomer.aspx");
+        }
+
+        protected void NewEmployee_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogNewEmployee.aspx");
+        }
+
+        protected void CustomerInfo_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogCustomerInfo.aspx");
+        }
+
+        protected void EmployeeInfo_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogEmployeeInfo.aspx");
+        }
+
+        protected void InventoryInfo_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogInvInfo.aspx");
+        }
+
+        protected void NewService_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogNewService.aspx");
+        }
+
+
+       
+
+        protected void ServiceInfo_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogServiceInfo.aspx");
+        }
+
+        protected void NewWorkFlow_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogNewWorkflow.aspx");
+        }
+        protected void WorkFlowInfo_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogWorkFlowInfo.aspx");
+        }
+        protected void NewItem_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogNewItem.aspx");
+        }
+        protected void equipMana_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("EquipmentManagement.aspx");
+        }
+        
+        protected void noteMgmt_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LoggedNotes.aspx");
+        }
+        protected void serviceEdit_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogServiceTicketEdit.aspx");
+        }
+
+        protected void servReq_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogServiceRequest.aspx");
+        }
+        protected void invAuction_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogInvToAuction.aspx");
+        }
+        protected void newAuction_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogCreateNewAuction.aspx");
+        }
+
+
+        protected void moveFormBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LogMoveFormInfo.aspx");
+        }
+    }
+}
