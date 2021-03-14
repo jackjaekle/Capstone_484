8e696ab6e5c4583299193310afafd6d4c123584a
manyxcm <38437169+manyxcm@users.noreply.github.com>
2021-03-14 13:45:56 -0400

    Add files via upload

--- Lab3/LogMoveFormInfo.aspx
+++ Lab3/LogMoveFormInfo.aspx
@@ -3,7 +3,7 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <asp:Label ID="Label1" runat="server" Text="Choose what move you would like information on"></asp:Label>
-    <asp:DropDownList ID="ddlMoveForm" runat="server" DataSourceID="dtaSrcMoveFormID" DataTextField="MoveFormID" AutoPostBack="true"></asp:DropDownList>
+    <asp:DropDownList ID="ddlMoveForm" runat="server"  DataTextField="MoveFormID" AutoPostBack="true"></asp:DropDownList>
     <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
     <fieldset>
         <legend>
@@ -11,16 +11,22 @@
         </legend>
         <asp:GridView ID="grdMoveForm" runat="server"></asp:GridView>
     </fieldset>
+
     <asp:Label ID="lblFloorInfo" runat="server" Text="View rooms for a specific floor: "></asp:Label>
     <asp:DropDownList ID="ddlFloor" runat="server" DataTextField="Floor"></asp:DropDownList>
     <asp:Button ID="btnShowFloor" runat="server" Text="Sort by floor" OnClick="btnShowFloor_Click"/>
-    <asp:Button ID="btnShowAll" runat="server" Text="Show all rooms" OnClick="btnShowAll_Click"/>
+    <asp:Button ID="btnShowAll" runat="server" Text="Show all Floors" OnClick="btnShowAll_Click"/>
+
     <fieldset>
         <legend>
             <asp:Label ID="lblRoomInfo" runat="server" Text="Rooms Information"></asp:Label>
         </legend>
-        <asp:GridView ID="grdRoomInfo" runat="server"></asp:GridView>
+        <asp:GridView ID="grdRoomInfo" runat="server">
+            <Columns>
+            </Columns>
+        </asp:GridView>
     </fieldset>
+
     <asp:Button ID="btnBack" runat="server" Text="Back" OnClick="btnBack_Click"/>
     <asp:SqlDataSource ID="dtaSrcMoveFormID" runat="server" ConnectionString="<%$ ConnectionStrings:Lab3 %>"  SelectCommand ="SELECT MoveFormID from MoveForm"></asp:SqlDataSource>
 </asp:Content>
--- Lab3/LogMoveFormInfo.aspx.cs
+++ Lab3/LogMoveFormInfo.aspx.cs
@@ -14,29 +14,73 @@ namespace Lab3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (ddlMoveForm.Items.Count == 0)
+            {
+                String sqlQuery = "Select s.ServiceName FROM Service s, MoveForm e WHERE s.ServiceID = e.ServiceID";
+                SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnect;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sqlQuery;
+                sqlConnect.Open();
+                SqlDataReader queryResults = sqlCommand.ExecuteReader();
+                while (queryResults.Read())
+                {
+                    ddlMoveForm.Items.Add(queryResults["ServiceName"].ToString());
+                   
 
+                }
+               
+                sqlConnect.Close();
+            }
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            String sqlQuery = "Select * FROM MoveForm WHERE ServiceID = " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
-            String sqlQuery2 = "SELECT * from Rooms WHERE MoveFormID = " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
-            String sqlQuery3 = "SELECT DISTINCT Floor from Rooms WHERE MoveFormID = " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
             SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
+
+
+            String sqlQuery4 = "SELECT ServiceID from Service where ServiceName = @name";
+            SqlCommand sqlCommand4 = new SqlCommand();
+            sqlCommand4.Connection = sqlConnect;
+            sqlCommand4.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
+            sqlCommand4.CommandType = CommandType.Text;
+            sqlCommand4.CommandText = sqlQuery4;
+            sqlConnect.Open();
+            int serviceID = (int)sqlCommand4.ExecuteScalar();
+            sqlConnect.Close();
+
+            String sqlQuery5 = "SELECT MoveFormID from MoveForm, Service where MoveForm.ServiceID = Service.ServiceID AND Service.ServiceName = @name";
+            SqlCommand sqlCommand5 = new SqlCommand();
+            sqlCommand5.Connection = sqlConnect;
+            sqlCommand5.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
+            sqlCommand5.CommandType = CommandType.Text;
+            sqlCommand5.CommandText = sqlQuery5;
+            sqlConnect.Open();
+            int workFormID = (int)sqlCommand5.ExecuteScalar();
+            sqlConnect.Close();
+
+            String sqlQuery = "Select e.Stories, e.TruckDist, e.DrivewayAccess, e.LoadCondition FROM MoveForm e, Service s WHERE s.ServiceID = e.ServiceID AND s.ServiceName = @name" ;
+            String sqlQuery2 = "SELECT r.Name, r.Floor, r.TypeofBoxes, r.NumOfBoxes, r.Blankets from Rooms r, Service s, MoveForm e WHERE r.MoveFormID = " + workFormID + " AND s.ServiceID = e.ServiceID AND s.ServiceName = @name";
+            String sqlQuery3 = "SELECT DISTINCT r.Floor from Rooms r, MoveForm e, Service s WHERE r.MoveFormID = e.MoveFormID AND e.ServiceID = " + serviceID;// s.ServiceID = e.ServiceID AND s.ServiceName = @name";
+
 
             SqlCommand sqlCommand = new SqlCommand();
             sqlCommand.Connection = sqlConnect;
+            sqlCommand.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
             sqlCommand.CommandType = CommandType.Text;
             sqlCommand.CommandText = sqlQuery;
 
             SqlCommand sqlCommand2 = new SqlCommand();
             sqlCommand2.Connection = sqlConnect;
+            sqlCommand2.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
             sqlCommand2.CommandType = CommandType.Text;
             sqlCommand2.CommandText = sqlQuery2;
 
 
             SqlCommand sqlCommand3 = new SqlCommand();
             sqlCommand3.Connection = sqlConnect;
+            sqlCommand3.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
             sqlCommand3.CommandType = CommandType.Text;
             sqlCommand3.CommandText = sqlQuery3;
 
@@ -58,8 +102,8 @@ namespace Lab3
             ddlFloor.DataBind();
             sqlConnect.Close();
 
-            lblMoveForm.Text = "MoveForm information for MoveForm " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
-            lblRoomInfo.Text = "Rooms Information for MoveForm " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
+            lblMoveForm.Text = "MoveForm information for Service " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
+            lblRoomInfo.Text = "Rooms Information for Service " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
         }
 
         protected void btnBack_Click(object sender, EventArgs e)
@@ -69,11 +113,12 @@ namespace Lab3
 
         protected void btnShowFloor_Click(object sender, EventArgs e)
         {
-            String sqlQuery = "Select * FROM Rooms WHERE MoveFormID = " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue) + " AND Floor = " + HttpUtility.HtmlEncode(ddlFloor.SelectedValue);
+            String sqlQuery = "Select r.Name, r.Floor, r.TypeofBoxes, r.NumOfBoxes, r.Blankets FROM Rooms r, Service e, MoveForm s WHERE s.ServiceID = e.ServiceID AND e.ServiceName = @name AND Floor = " + HttpUtility.HtmlEncode(ddlFloor.SelectedValue);
             SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
 
             SqlCommand sqlCommand = new SqlCommand();
             sqlCommand.Connection = sqlConnect;
+            sqlCommand.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
             sqlCommand.CommandType = CommandType.Text;
             sqlCommand.CommandText = sqlQuery;
 
@@ -83,15 +128,27 @@ namespace Lab3
             grdRoomInfo.DataBind();
             sqlConnect.Close();
 
-            lblRoomInfo.Text = "Rooms Information for MoveForm " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue) + ", Floor " + HttpUtility.HtmlEncode(ddlFloor.SelectedValue);
+            lblRoomInfo.Text = "Rooms Information for Service " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue) + ", Floor " + HttpUtility.HtmlEncode(ddlFloor.SelectedValue);
         }
 
         protected void btnShowAll_Click(object sender, EventArgs e)
         {
-            String sqlQuery = "Select * FROM Rooms WHERE MoveFormID = " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
             SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
 
+            String sqlQuery2 = "SELECT MoveFormID from MoveForm, Service where MoveForm.ServiceID = Service.ServiceID AND Service.ServiceName = @name";
+            SqlCommand sqlCommand2 = new SqlCommand();
+            sqlCommand2.Connection = sqlConnect;
+            sqlCommand2.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
+            sqlCommand2.CommandType = CommandType.Text;
+            sqlCommand2.CommandText = sqlQuery2;
+            sqlConnect.Open();
+            int workFormID = (int)sqlCommand2.ExecuteScalar();
+            sqlConnect.Close();
+
+            String sqlQuery = "Select r.Name, r.Floor, r.TypeofBoxes, r.NumOfBoxes, r.Blankets FROM Rooms r, Service e, MoveForm s WHERE r.MoveFormID = " + workFormID + " AND s.ServiceID = e.ServiceID AND e.ServiceName = @name";
+
             SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
             sqlCommand.Connection = sqlConnect;
             sqlCommand.CommandType = CommandType.Text;
             sqlCommand.CommandText = sqlQuery;
@@ -102,7 +159,7 @@ namespace Lab3
             grdRoomInfo.DataBind();
             sqlConnect.Close();
 
-            lblRoomInfo.Text = "Rooms Information for MoveForm " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
+            lblRoomInfo.Text = "Rooms Information for Service " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
         }
     }
 }
