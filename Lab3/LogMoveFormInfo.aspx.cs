d92f2d748d3cee24bcff0f5997c4d8ccc6bc86cc
manyxcm <38437169+manyxcm@users.noreply.github.com>
2021-03-26 20:21:57 -0400

    Add files via upload

--- Lab3/LogMoveFormInfo.aspx.cs
+++ Lab3/LogMoveFormInfo.aspx.cs
@@ -14,8 +14,13 @@ namespace Lab3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+
             if (ddlMoveForm.Items.Count == 0)
             {
+                //dtaSrcMoveFormID.SelectCommand = "Select s.ServiceName, e.MoveFormID FROM Service s, MoveForm e WHERE s.ServiceID = e.ServiceID";
+                //ddlMoveForm.DataTextField = "ServiceName";
+                //ddlMoveForm.DataValueField = "MoveFormID";
                 String sqlQuery = "Select s.ServiceName FROM Service s, MoveForm e WHERE s.ServiceID = e.ServiceID";
                 SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
                 SqlCommand sqlCommand = new SqlCommand();
@@ -27,45 +32,30 @@ namespace Lab3
                 while (queryResults.Read())
                 {
                     ddlMoveForm.Items.Add(queryResults["ServiceName"].ToString());
-                   
+
 
                 }
-               
+
                 sqlConnect.Close();
             }
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
+            String sqlQuery = "Select e.Stories, e.TruckDist, e.DrivewayAccess, e.LoadCondition FROM MoveForm e, Service s WHERE " +
+                "s.ServiceID = e.ServiceID " +
+                "AND s.ServiceName = @name" ;
+            String sqlQuery2 = "SELECT r.Name, r.Floor, r.TypeofBoxes, r.NumOfBoxes, r.Blankets from Rooms r, Service s, MoveForm e WHERE " +
+                "s.ServiceID = e.ServiceID " +
+                "AND s.ServiceName = @name " +
+                "AND r.MoveFormID = e.MoveFormID";
+            String sqlQuery3 = "SELECT DISTINCT r.Floor from Rooms r, MoveForm e, Service s WHERE " +
+                "s.ServiceID = e.ServiceID " +
+                "AND s.ServiceName = @name";
             SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
 
-
-            String sqlQuery4 = "SELECT ServiceID from Service where ServiceName = @name";
-            SqlCommand sqlCommand4 = new SqlCommand();
-            sqlCommand4.Connection = sqlConnect;
-            sqlCommand4.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
-            sqlCommand4.CommandType = CommandType.Text;
-            sqlCommand4.CommandText = sqlQuery4;
-            sqlConnect.Open();
-            int serviceID = (int)sqlCommand4.ExecuteScalar();
-            sqlConnect.Close();
-
-            String sqlQuery5 = "SELECT MoveFormID from MoveForm, Service where MoveForm.ServiceID = Service.ServiceID AND Service.ServiceName = @name";
-            SqlCommand sqlCommand5 = new SqlCommand();
-            sqlCommand5.Connection = sqlConnect;
-            sqlCommand5.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
-            sqlCommand5.CommandType = CommandType.Text;
-            sqlCommand5.CommandText = sqlQuery5;
-            sqlConnect.Open();
-            int workFormID = (int)sqlCommand5.ExecuteScalar();
-            sqlConnect.Close();
-
-            String sqlQuery = "Select e.Stories, e.TruckDist, e.DrivewayAccess, e.LoadCondition FROM MoveForm e, Service s WHERE s.ServiceID = e.ServiceID AND s.ServiceName = @name" ;
-            String sqlQuery2 = "SELECT r.Name, r.Floor, r.TypeofBoxes, r.NumOfBoxes, r.Blankets from Rooms r, Service s, MoveForm e WHERE r.MoveFormID = " + workFormID + " AND s.ServiceID = e.ServiceID AND s.ServiceName = @name";
-            String sqlQuery3 = "SELECT DISTINCT r.Floor from Rooms r, MoveForm e, Service s WHERE r.MoveFormID = e.MoveFormID AND e.ServiceID = " + serviceID;// s.ServiceID = e.ServiceID AND s.ServiceName = @name";
-
-
             SqlCommand sqlCommand = new SqlCommand();
+            
             sqlCommand.Connection = sqlConnect;
             sqlCommand.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
             sqlCommand.CommandType = CommandType.Text;
@@ -104,6 +94,8 @@ namespace Lab3
 
             lblMoveForm.Text = "MoveForm information for Service " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
             lblRoomInfo.Text = "Rooms Information for Service " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
+            btnShowFloor.Enabled = true;
+            btnShowAll.Enabled = true;
         }
 
         protected void btnBack_Click(object sender, EventArgs e)
@@ -113,7 +105,7 @@ namespace Lab3
 
         protected void btnShowFloor_Click(object sender, EventArgs e)
         {
-            String sqlQuery = "Select r.Name, r.Floor, r.TypeofBoxes, r.NumOfBoxes, r.Blankets FROM Rooms r, Service e, MoveForm s WHERE s.ServiceID = e.ServiceID AND e.ServiceName = @name AND Floor = " + HttpUtility.HtmlEncode(ddlFloor.SelectedValue);
+            String sqlQuery = "Select r.Name, r.Floor, r.TypeofBoxes, r.NumOfBoxes, r.Blankets FROM Rooms r, Service e, MoveForm s WHERE s.ServiceID = e.ServiceID AND r.MoveFormID = s.MoveFormID AND e.ServiceName = @name AND Floor = " + HttpUtility.HtmlEncode(ddlFloor.SelectedValue);
             SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
 
             SqlCommand sqlCommand = new SqlCommand();
@@ -127,25 +119,21 @@ namespace Lab3
             grdRoomInfo.DataSource = queryResults;
             grdRoomInfo.DataBind();
             sqlConnect.Close();
-
             lblRoomInfo.Text = "Rooms Information for Service " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue) + ", Floor " + HttpUtility.HtmlEncode(ddlFloor.SelectedValue);
+
+            fillRoomItemsDDL();
+
+            btnSubmitItems.Enabled = true;
+            btnAddNewItem.Enabled = true;
         }
 
         protected void btnShowAll_Click(object sender, EventArgs e)
         {
+            String sqlQuery = "Select r.Name, r.Floor, r.TypeofBoxes, r.NumOfBoxes, r.Blankets FROM Rooms r, Service e, MoveForm s WHERE " +
+                "s.ServiceID = e.ServiceID " +
+                "AND e.ServiceName = @name " +
+                "AND r.MoveFormID = s.MoveFormID";
             SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
-
-            String sqlQuery2 = "SELECT MoveFormID from MoveForm, Service where MoveForm.ServiceID = Service.ServiceID AND Service.ServiceName = @name";
-            SqlCommand sqlCommand2 = new SqlCommand();
-            sqlCommand2.Connection = sqlConnect;
-            sqlCommand2.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
-            sqlCommand2.CommandType = CommandType.Text;
-            sqlCommand2.CommandText = sqlQuery2;
-            sqlConnect.Open();
-            int workFormID = (int)sqlCommand2.ExecuteScalar();
-            sqlConnect.Close();
-
-            String sqlQuery = "Select r.Name, r.Floor, r.TypeofBoxes, r.NumOfBoxes, r.Blankets FROM Rooms r, Service e, MoveForm s WHERE r.MoveFormID = " + workFormID + " AND s.ServiceID = e.ServiceID AND e.ServiceName = @name";
 
             SqlCommand sqlCommand = new SqlCommand();
             sqlCommand.Parameters.AddWithValue("name", HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue));
@@ -160,6 +148,88 @@ namespace Lab3
             sqlConnect.Close();
 
             lblRoomInfo.Text = "Rooms Information for Service " + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue);
+
+            fillRoomItemsDDL();
+
+            btnSubmitItems.Enabled = true;
+            btnAddNewItem.Enabled = true;
+
+        }
+
+        protected void btnSubmitItems_Click(object sender, EventArgs e)
+        {
+            refreshRoomItems();
+            //lblItemInfo.Text = "Item Information for Room " + HttpUtility.HtmlEncode(ddlRooms.SelectedValue) + ", Floor " + HttpUtility.HtmlEncode(ddlFloor.SelectedValue);
+        }
+
+        protected void btnAddNewItem_Click(object sender, EventArgs e)
+        {
+            int roomID = int.Parse(ddlRooms.SelectedValue);
+            String Name = HttpUtility.HtmlEncode(txtItemName.Text);
+            String Description = HttpUtility.HtmlEncode(txtItemDescrip.Text);
+            String Action;
+            if (rdoBtnMove.Checked)
+            {
+                Action = "Move";
+            }
+            else
+            {
+                Action = "Dispose";
+            }
+            String sqlQuery = "Insert into RoomItems (RoomID, Name, Description, Action) " +
+                "values (@roomID, @name ,@description, @action);";
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
+
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Parameters.AddWithValue("roomID", roomID);
+            sqlCommand.Parameters.AddWithValue("name", Name);
+            sqlCommand.Parameters.AddWithValue("description", Description);
+            sqlCommand.Parameters.AddWithValue("action", Action);
+
+            sqlCommand.Connection = sqlConnect;
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.CommandText = sqlQuery;
+
+            sqlConnect.Open();
+            SqlDataReader queryResults = sqlCommand.ExecuteReader();
+            queryResults.Close();//closes connection
+            sqlConnect.Close();
+
+            txtItemDescrip.Text = string.Empty;
+            txtItemName.Text = string.Empty;
+            rdoBtnDispose.Checked = false;
+            rdoBtnMove.Checked = true;
+
+            refreshRoomItems();
+        }
+
+        protected void refreshRoomItems()
+        {
+            String sqlQuery = "Select distinct r.Name, r.Description, r.Action FROM RoomItems r, Rooms ro WHERE r.RoomID = " + HttpUtility.HtmlEncode(ddlRooms.SelectedValue);
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
+
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnect;
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.CommandText = sqlQuery;
+
+            sqlConnect.Open();
+            SqlDataReader queryResults = sqlCommand.ExecuteReader();
+            grdViewItems.DataSource = queryResults;
+            grdViewItems.DataBind();
+            sqlConnect.Close();
+        }
+
+        protected void fillRoomItemsDDL()
+        {
+            //Not a parameterized query bc it doesn't liek to rerun with the same name when you change the floor you're looking at
+            //Shouldn't matter bc the only input is from ddl's anyway, encoded too.
+            dtaSrcRoomItemsID.SelectCommand = "Select r.Name, r.RoomID FROM Rooms r, Service e, MoveForm s WHERE s.ServiceID = e.ServiceID" +
+                " AND e.ServiceName = '" + HttpUtility.HtmlEncode(ddlMoveForm.SelectedValue) +
+                "' AND r.MoveFormID = s.MoveFormID";
+            ddlRooms.DataTextField = "Name";
+            ddlRooms.DataValueField = "RoomID";
         }
     }
 }
--- Lab3/LogMoveFormInfo.aspx.designer.cs
+++ Lab3/LogMoveFormInfo.aspx.designer.cs
@@ -114,6 +114,123 @@ namespace Lab3
         protected global::System.Web.UI.WebControls.GridView grdRoomInfo;
 
         /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// ddlRooms control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlRooms;
+
+        /// <summary>
+        /// btnSubmitItems control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmitItems;
+
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
+        /// txtItemName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtItemName;
+
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// txtItemDescrip control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtItemDescrip;
+
+        /// <summary>
+        /// rdoBtnMove control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdoBtnMove;
+
+        /// <summary>
+        /// rdoBtnDispose control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdoBtnDispose;
+
+        /// <summary>
+        /// btnAddNewItem control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddNewItem;
+
+        /// <summary>
+        /// lblItemInfo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblItemInfo;
+
+        /// <summary>
+        /// grdViewItems control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grdViewItems;
+
+        /// <summary>
         /// btnBack control.
         /// </summary>
         /// <remarks>
@@ -130,5 +247,14 @@ namespace Lab3
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource dtaSrcMoveFormID;
+
+        /// <summary>
+        /// dtaSrcRoomItemsID control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource dtaSrcRoomItemsID;
     }
 }
